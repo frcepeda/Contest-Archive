a53adcd41205792c05cb6e091cc81f42ed274eda
Freddy Román <freddy@frcepeda.com>
2012-04-08 16:08:11 -0500

    Finished Section 1.3.

---
+++ C/USACO Training/Section 1/Section 1.3/calfflac.c
@@ -0,0 +1,109 @@
+/*
+    ID: frceped1
+    LANG: C
+    TASK: calfflac
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <strings.h>
+
+#define UPLOAD 1
+
+#if UPLOAD
+	#define read(...) fscanf(in, __VA_ARGS__)
+	#define print(...) fprintf(out, __VA_ARGS__)
+	#define getLine(buf) fgets(buf, sizeof(buf), in);
+	#define openFiles() in = fopen("calfflac.in", "r"); out = fopen("calfflac.out", "w")
+	#define getchar() fgetc(in)
+	#define putchar(c) fputc(c, out)
+#else
+	#define read(...) scanf(__VA_ARGS__)
+	#define print(...) printf(__VA_ARGS__)
+	#define getLine(buf) fgets(buf, sizeof(buf), stdin);
+	#define openFiles()
+#endif
+
+FILE *in, *out;
+
+// USACO upload macros
+
+#define MAXLENGTH 20000
+
+char originalString[MAXLENGTH];
+char noPunctuationString[MAXLENGTH];
+int originalLength, noPunctuationLength;
+
+int main(void){
+	openFiles();
+
+	char c;
+
+	while ((c = getchar()) != EOF){
+		originalString[originalLength++] = c;
+		if (isalpha(c))
+			noPunctuationString[noPunctuationLength++] = tolower(c);
+	}
+
+	int i;
+	int maxLen, maxi;
+	int start, end, len;
+	int center, centered;
+	for (i = 0; i < noPunctuationLength * 2; i++){
+		if (i % 2 == 0){
+			start = end = i / 2;
+			start--;
+			end++;
+			len = 1;
+		} else {
+			start = (i - 1) / 2;
+			end = (i + 1) / 2;
+			len = 0;
+		}
+		center = start;
+		while (start >= 0 &&
+				end < noPunctuationLength &&
+				noPunctuationString[start] == noPunctuationString[end]){
+			start--;
+			end++;
+			len += 2;
+		}
+		if (len > maxLen){
+			maxLen = len;
+			maxi = i;
+			centered = (i%2==0) ? 1 : 0;
+		}
+	}
+
+
+	print("%d\n", maxLen);
+
+	if (i % 2 == 0){
+		start = end = i / 2;
+		start--;
+		end++;
+		len = 1;
+	} else {
+		start = (i - 1) / 2;
+		end = (i + 1) / 2;
+		len = 0;
+	}
+
+	i = -1;
+	maxi /= 2;
+	maxi += centered ? 1 : 2;
+	maxi -= maxLen / 2;
+	while (maxi){
+		i++;
+		if (isalpha(originalString[i])) maxi--;
+	}
+
+	while (maxLen){
+		putchar(originalString[i]);
+		if (isalpha(originalString[i++])) maxLen--;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
---
+++ C/USACO Training/Section 1/Section 1.3/crypt1.c
@@ -0,0 +1,90 @@
+/*
+    ID: frceped1
+    LANG: C
+    TASK: crypt1
+ */
+
+#include <stdio.h>
+
+#define UPLOAD 1
+
+#if UPLOAD
+	#define read(...) fscanf(in, __VA_ARGS__)
+	#define print(...) fprintf(out, __VA_ARGS__)
+	#define getLine(buf) fgets(buf, sizeof(buf), in);
+	#define openFiles() in = fopen("crypt1.in", "r"); out = fopen("crypt1.out", "w")
+	#define getchar() fgetc(in)
+	#define putchar(c) fputc(c, out)
+#else
+	#define read(...) scanf(__VA_ARGS__)
+	#define print(...) printf(__VA_ARGS__)
+	#define getLine(buf) fgets(buf, sizeof(buf), stdin);
+	#define openFiles()
+#endif
+
+FILE *in, *out;
+
+// USACO upload macros
+
+#define MAXNUM 10
+
+int inSet[MAXNUM];
+int toCheckNum;
+
+int digitCount(int num){
+	const int base = 10;
+	if (!num) return 1;
+	int count = 0;
+	while (num){
+		num /= base;
+		count++;
+	}
+	return count;
+}
+
+int test(int num){
+	const int base = 10;
+	int digit;
+	while (num){
+		digit = num % base;
+		num /= base;
+		if (!inSet[digit]) return 0;
+	}
+	return 1;
+}
+
+int try(int up1, int up2, int up3, int down1, int down2){
+	if (!(test(up1) && test(up2) && test(up3) && test(down1) && test(down2))) return 0;
+	int up = up1 + up2 * 10 + up3 * 100;
+	int down = down1 + down2 * 10;
+	int partial1 = down1 * up;
+	int partial2 = down2 * up;
+	int answer = up * down;
+	return test(partial1) && test(partial2) && test(answer) &&
+		digitCount(partial1) <= 3 && digitCount(partial2) <= 3 && digitCount(answer) <= 4;
+}
+
+int main(void){
+	openFiles();
+	int i,j,k,l,m;
+	int answer = 0;
+
+	read("%d", &toCheckNum);
+
+	for (i = 0; i < toCheckNum; i++){
+		read("%d", &j);
+		inSet[j] = 1;
+	}
+
+	for (i = 1; i <= 10; i++)
+		for (j = 1; j <= 10; j++)
+			for (k = 1; k <= 10; k++)
+				for (l = 1; l <= 10; l++)
+					for (m = 1; m <= 10; m++)
+						if (try(i,j,k,l,m))
+							answer++;
+
+	print("%d\n", answer);
+
+	return 0;
+}
