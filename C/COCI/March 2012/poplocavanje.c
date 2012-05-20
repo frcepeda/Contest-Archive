eafd76b22420b783b5387be5d50f6c90b4e3e0a4
Freddy Román <freddy@frcepeda.com>
2012-05-20 17:02:47 -0500

    Modified poplocavanje to use Aho-Corasik.

--- C/COCI/March 2012/poplocavanje.c
+++ C/COCI/March 2012/poplocavanje.c
@@ -1,67 +1,194 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
 
-#define MAXSTREET 3000001
-#define MAXTILES 5001
+#define MAXNODES 25000001
+#define MAXSTRING 300001
+#define MAXTILE 5001
+#define CHILDNUM 4
 
-char street[MAXSTREET];
-int streetlen;
-int oktiles[MAXSTREET];
-
-char tiles[MAXTILES][MAXTILES];
-char tilenum;
-
-void delNewline(char *str){
-	int i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
-		i++;
-	str[i] = '\0';
+int max(int a, int b) {
+	return a > b ? a : b;
 }
 
-int mycomp(char *a, char *b){
+typedef struct _node node;
+struct _node {
+	char c;
+	node *children[CHILDNUM]; // trie
+	node *next; // for linked list
+	node *fall; // when matching fails
+	node *parent;
+	bool isEnd;
+	int length;
+};
+
+void addChild(node *curr, node *child){
+	node *last = curr->children[child->c % CHILDNUM];
+
+	if (last == NULL){
+		curr->children[child->c % CHILDNUM] = child;
+	} else {
+		while (last->next != NULL)
+			last = last->next;
+
+		last->next = child;
+	}
+
+	child->parent = curr;
+}
+
+node *findChild(node *curr, char c){
+	curr = curr->children[c % CHILDNUM];
+
+	while (curr != NULL){
+		if (curr->c == c)
+			return curr;
+		else
+			curr = curr->next;
+	}
+
+	return NULL;
+}
+
+node storage[MAXNODES];
+int nextNode = 1;
+node *root = &storage[0];
+
+node *newNode(char c){
+	storage[nextNode].c = c;
+	return &storage[nextNode++];
+}
+
+void addStringToNode(node *curr, char *str, int len){
+	char c = *str;
+
+	if (!c){
+		curr->isEnd = true;
+		curr->length = len;
+		return;
+	}
+	
+	node *next = findChild(curr, c);
+
+	if (next == NULL){
+		next = newNode(c);
+		addChild(curr, next);
+	}
+
+	addStringToNode(next, str+1, len+1);
+}
+
+void addString(char *str){
+	addStringToNode(root, str, 0);
+}
+
+node *queue[MAXNODES];
+int qs, qe;
+
+#define queueIsNotEmpty qs < qe
+
+void push(node *n){
+	queue[qe++] = n;
+}
+
+node *pop(void){
+	return queue[qs++];
+}
+
+void constructAhoCorasik(void){
 	int i;
-	for (i = 0; a[i] && b[i]; i++) {
-		if (a[i] != b[i]) {
-			return 1;
+
+	root->fall = root;
+	push(root);
+
+	while(queueIsNotEmpty) {
+		node *curr = pop();
+
+		for (i = 0; i < CHILDNUM; i++){
+			node *child = curr->children[i];
+			while (child != NULL){
+				push(child);
+				child = child->next;
+			}
+		}
+
+		if (curr == root) continue;
+
+		node *fall = curr->parent->fall;
+
+		while (fall != root && findChild(fall, curr->c) == NULL)
+			fall = fall->fall;
+
+		curr->fall = findChild(fall, curr->c);
+
+		if (curr->fall == NULL || curr->fall == curr)
+			curr->fall = root;
+	}
+}
+
+int tiled[MAXSTRING];
+
+void tile(int start, int length){
+	int i = start-length+1;
+	for (; i <= start; i++)
+		tiled[i] = true;
+}
+
+void searchInString(char *str){
+	int i;
+	char c;
+	node *state = root;
+	for (i = 0; str[i]; i++){
+		c = str[i];
+
+		while (state != root && findChild(state, c) == NULL)
+			state = state->fall;
+
+		state = findChild(state, c);
+
+		if (state == NULL){
+			state = root;
+			continue;
+		}
+
+		node *report = state;
+
+		while (report != root){
+			if (report->isEnd){
+				tiled[i-report->length+1] = max(tiled[i-report->length+1], report->length);
+			}
+			report = report->fall;
 		}
 	}
-	if (a[i]) {
-		return 1;
+}
+
+int main(void){
+	int i;
+	char searchString[MAXSTRING];
+	char tileString[MAXTILE];
+
+	scanf("%*d");
+	scanf("%s", searchString);
+	scanf("%d", &i);
+
+	while (i--){
+		scanf("%s", tileString);
+		addString(tileString);
 	}
+
+	constructAhoCorasik();
+
+	searchInString(searchString);
+
+	int answer = 0;
+	for (i = 0; searchString[i]; i++){
+		if (!tiled[i])
+			answer++;
+		else
+			i += tiled[i];
+	}
+
+	printf("%d\n", answer);
+
 	return 0;
 }
 
-int main(int argc, const char * argv[])
-{
-	int i;
-	scanf("%d%*[ \n\r]", &streetlen);
-	fgets(street, MAXSTREET, stdin);
-	delNewline(street);
-	scanf("%d%*[ \n\r]", &tilenum);
-	if (tilenum * 5000 * streetlen > 100000000) {
-		printf("%d\n", streetlen);
-		return 0;
-	}
-	for (i = 0; i < tilenum; i++) {
-		fgets(tiles[i], MAXTILES, stdin);
-		delNewline(tiles[i]);
-	}
-	for (i = 0; i < streetlen; i++) {
-		int j;
-		for (j = 0; j < tilenum; j++) {
-			if (mycomp(tiles[j], &street[i]) == 0) {
-				int l;
-				for (l = 0; l < strlen(tiles[j]); l++) {
-					oktiles[i+l] = 1;
-				}
-			}
-		}
-	}
-	int answer = streetlen;
-	for (i = 0; i < streetlen; i++) {
-		if (oktiles[i]) {
-			answer--;
-		}
-	}
-	printf("%d\n", answer);
-}
