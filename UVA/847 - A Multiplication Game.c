5948f26ca835a24da83dcf0dfde90b0c9c3f51fd
Freddy Román <me@freddy.mx>
2013-01-25 20:11:16 -0600

    Added A Multiplication Game.

---
+++ UVA/847 - A Multiplication Game.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main(void){
+	unsigned int N;
+
+	while (scanf("%d", &N) != EOF){
+		bool wins = true;
+
+		while (N != 1){
+			N = ceil((double)N/(wins ? 9 : 2));
+			wins ^= true;
+		}
+
+		printf("%s wins.\n", wins ? "Ollie" : "Stan");
+	}
+
+	return 0;
+}
