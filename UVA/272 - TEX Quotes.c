6a30f9c85c4670781c382cb43bcda4ff8092bcbe
Freddy Román <me@freddy.mx>
2013-05-19 23:12:34 -0500

    Added UVA's 272 - TEX Quotes.

---
+++ UVA/272 - TEX Quotes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	int first = 1;
+	char c;
+
+	while ((c = getchar()) != EOF) {
+		if (c != '\"')
+			putchar(c);
+		else {
+			printf("%s", first ? "``" : "''");
+			first ^= 1;
+		}
+	}
+
+	return 0;
+}
