1bafa9501a6c2858ff510f086b1d1d300a5ff26c
Freddy Román <me@freddy.mx>
2013-06-02 11:13:06 -0500

    Updated Longest Collatz sequence.

--- Project Euler/Longest Collatz sequence.hs
+++ Project Euler/Longest Collatz sequence.hs
@@ -1,19 +1,18 @@
+import Data.List
+
 collatz' :: Integer -> Integer -> Integer
 collatz' acc n
-	| n == 1         = acc
-	| n `mod` 2 == 0 = collatz' (acc+1) (n `div` 2)
-	| otherwise      = collatz' (acc+1) (3 * n + 1)
+        | n == 1         = acc
+        | n `mod` 2 == 0 = collatz' (acc+1) (n `div` 2)
+        | otherwise      = collatz' (acc+1) (3 * n + 1)
 
 collatz :: Integer -> Integer
 collatz n = collatz' 0 n
 
-argmax' :: (Ord b) => (a,b) -> (a -> b) -> [a] -> (a,b)
-argmax' best f [] = best
-argmax' (b,fb) f (x:xs) = let fx = f x
-                              a = if fx > fb then (x,fx) else (b,fb)
-                              in argmax' a f xs
-
 argmax :: (Ord b) => (a -> b) -> [a] -> a
-argmax f (x:xs) = fst $ argmax' (x, f x) f xs
+argmax f (x:xs) = fst $ foldl' step (x, f x) xs
+                  where step (b,fb) curr = let a = curr
+                                               fa = f a
+                                           in if fa > fb then (a,fa) else (b,fb)
 
 main = print $ argmax collatz [1..10^6]
