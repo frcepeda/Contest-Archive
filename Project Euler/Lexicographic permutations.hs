ab55cbccc34a5ce6eef9c586f964ee374029b232
Freddy Román <me@freddy.mx>
2013-06-05 12:06:13 -0500

    Updated Lexicographic permutations.

--- Project Euler/Lexicographic permutations.hs
+++ Project Euler/Lexicographic permutations.hs
@@ -1,3 +1,13 @@
 import Data.List
--- I know I'm lazy, but it runs in like 11 seconds, so meh.
-main = print $ ((!!999999) . sort . permutations) [0..9]
+
+kthPermutation k xs = buildPermutation xs (factoradic [] k 1)
+                      where n = length xs
+                            factoradic acc k i
+                                       | i > n     = acc
+                                       | otherwise = factoradic (next:acc) (k `div` i) (i+1)
+                                                     where next = k `mod` i
+                            buildPermutation [] _ = []
+                            buildPermutation xs (i:is) = next : buildPermutation (delete next xs) is
+                                                         where next = xs !! i
+
+main = print $ kthPermutation 999999 [0..9]
