a8a4d97ba7a6ace54bcc1ea9ff6c6fc4d3b77fab
Freddy Román <me@freddy.mx>
2013-09-07 01:40:15 -0500

    Added UVA problems 100 and 11498.

---
+++ UVA/100 - The 3n + 1 problem.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAXNUM 3000000
+#define MAXJ 1000000
+
+int cLength[MAXNUM] = {0,1};
+
+int collatz(unsigned int i){
+	if (i < MAXNUM){
+		if (cLength[i])
+			return cLength[i];
+		else if (i % 2 == 0)
+			return cLength[i] = 1 + collatz(i/2);
+		else
+			return cLength[i] = 1 + collatz(3*i+1);
+	} else {
+		if (i % 2 == 0)
+			return 1 + collatz(i/2);
+		else
+			return 1 + collatz(3*i+1);
+	}
+}
+
+int main(void){
+	int i, j;
+
+	for (i = 1; i < MAXJ; i++)
+		collatz(i);
+
+	while (scanf("%d %d", &i, &j) != EOF){
+		int k, m = 0;
+
+		for (k = i; k <= j; k++)
+			if (cLength[k] > m)
+				m = cLength[k];
+
+		for (k = j; k <= i; k++)
+			if (cLength[k] > m)
+				m = cLength[k];
+
+		printf("%d %d %d\n", i, j, m);
+	}
+
+	return 0;
+}
---
+++ UVA/11498 - Division of Nlogonia.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+	int K, N, M, X, Y;
+
+	while (scanf("%d", &K), K){
+		scanf("%d %d", &N, &M);
+
+		while (K--){
+			scanf("%d %d", &X, &Y);
+			X -= N;
+			Y -= M;
+
+			if (X == 0 || Y == 0)
+				printf("divisa\n");
+			else if (X > 0 && Y > 0)
+				printf("NE\n");
+			else if (X < 0 && Y > 0)
+				printf("NO\n");
+			else if (X < 0 && Y < 0)
+				printf("SO\n");
+			else
+				printf("SE\n");
+		}
+	}
+
+	return 0;
+}
