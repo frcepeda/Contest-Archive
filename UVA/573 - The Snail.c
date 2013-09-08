84392c00943c8af27b7eab7af128ea913b7cbb25
Freddy Román <me@freddy.mx>
2013-09-08 00:10:17 -0500

    Added UVA problem 573.

---
+++ UVA/573 - The Snail.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int H, U, D, F;
+
+int main(void) {
+	int i, d;
+	
+	while (scanf("%d %d %d %d", &H, &U, &D, &F), H){
+		H *= 100;
+		U *= 100;
+		D *= 100;
+		F = U*F/100;
+		i = 0;
+		for (d = 0; i >= 0 && i <= H; d++){
+			i += U;
+			if (i > H) break;
+			i -= D;
+			U -= F;
+			if (U < 0) U = 0;
+		}
+		printf("%s on day %d\n", i < 0 ? "failure" : "success",
+		                         d + (i>0) );
+	}
+	
+	return 0;
+}
