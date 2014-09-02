4170ccfe5ecbd2ea14f5f49835fd43543db2ed0e
Freddy Román <me@freddy.mx>
2014-09-01 21:24:01 -0500

    Added omegaUp's XOR.

--- omegaUp/XOR.c
+++ omegaUp/XOR.c
@@ -1,51 +1,23 @@
 #include <stdio.h>
 
-#define MAXN 2000200
+#define MAXN (1<<21)
 
-typedef struct {
-	int node, next;
-	unsigned cost;
-} edge;
-
-int N, M;
-edge tree[MAXN];
-int start[MAXN], nextNode = 1;
-unsigned xor[MAXN];
-
-void addEdge(unsigned a, unsigned b, unsigned c){
-	tree[nextNode].node = b;
-	tree[nextNode].cost = c;
-	tree[nextNode].next = start[a];
-	start[a] = nextNode++;
-}
-
-int f(int n, int p){
-	int i;
-	for (i = start[n]; i; i = tree[i].next){
-		if (tree[i].node == p) continue;
-		xor[tree[i].node] = xor[n] ^ tree[i].cost;
-		f(tree[i].node, n);
-	}
-}
+int cnt[MAXN] = {1};
+int N;
 
 int main(void){
-	int i;
+	int acc = 0, i, x, ans = 0;
 
-	scanf("%d %d", &N, &M);
-	for (i = 1; i < N; i++){
-		unsigned int a, b, c;
-		scanf("%u %u %u", &a, &b, &c);
-		addEdge(a, b, c);
-		addEdge(b, a, c);
+	scanf("%d", &N);
+
+	while (N--){
+		scanf("%d", &x);
+		acc ^= x;
+		ans += cnt[acc];
+		cnt[acc]++;
 	}
 
-	f(1, 0);
-
-	while (M--){
-		int a, b;
-		scanf("%d %d", &a, &b);
-		printf("%u\n", xor[a] ^ xor[b]);
-	}
+	printf("%d\n", ans);
 
 	return 0;
 }
