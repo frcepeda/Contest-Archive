e12abaff13374c4284aaf87ca5114b36dd6f2755
Freddy Román <me@freddy.mx>
2015-01-03 16:43:23 -0600

    Fixed ICPC Live Archive problem 6827
    Changed from single case to multicase.

--- ACM-ICPC Live Archive/6827 - Galaxy collision.cpp
+++ ACM-ICPC Live Archive/6827 - Galaxy collision.cpp
@@ -20,7 +20,7 @@ int start[MAXN], nextEdge = 1;
 
 typedef pair<int,int> pii;
 
-int N, ans;
+int N;
 pii P[MAXN];
 set<pair<pii,int> > in;
 
@@ -31,10 +31,10 @@ void addEdge(int a, int b){
 }
 
 int cnt, a;
-int color[MAXN];
+bool v[MAXN];
 void bipartite(int n, int c){
-	if (color[n]) return;
-	color[n] = 1;
+	if (v[n]) return;
+	v[n] = true;
 	if (c) a++;
 	cnt++;
 	for (int i = start[n]; i; i = graph[i].next)
@@ -44,32 +44,38 @@ void bipartite(int n, int c){
 int main(){
 	int i, j;
 
-	scanf("%d", &N);
-
-	for (i = 0; i < N; i++)
-		scanf("%d %d", &P[i].fst, &P[i].snd);
-
-	sort(P, P+N);
-
-	for (i = j = 0; i < N; i++){
-		for (; j < i && P[j].fst < P[i].fst - 5; j++)
-			in.erase(mp(mp(P[j].snd, P[j].fst), j));
-
-		for (set<pair<pii,int> >::iterator it = in.lower_bound(mp(mp(P[i].snd - 5,-1), -1)); it != in.end() && it->fst.fst <= P[i].snd + 5; it++)
-			if (sq(P[i].fst - it->fst.snd) + sq(P[i].snd - it->fst.fst) <= 25){
-				addEdge(i, it->snd);
-				addEdge(it->snd, i);
-			}
-
-		in.insert(mp(mp(P[i].snd, P[i].fst), i));
-	}
-
-	for (i = 0; i < N; i++)
-		if (!color[i]){
-			cnt = a = 0;
-			bipartite(i, 0);
-			ans += min(a, cnt - a);
+	while (scanf("%d", &N) != EOF){
+		fill(v, v + N, false);
+		fill(start, start + N, 0);
+		nextEdge = 1;
+		in.clear();
+		
+		for (i = 0; i < N; i++)
+			scanf("%d %d", &P[i].fst, &P[i].snd);
+	
+		sort(P, P+N);
+		
+		for (i = j = 0; i < N; i++){
+			for (; j < i && P[j].fst < P[i].fst - 5; j++)
+				in.erase(mp(mp(P[j].snd, P[j].fst), j));
+	
+			for (auto it = in.lower_bound(mp(mp(P[i].snd - 5,-1), -1)); it != in.end() && it->fst.fst <= P[i].snd + 5; it++)
+				if (sq(P[i].fst - it->fst.snd) + sq(P[i].snd - it->fst.fst) <= 25){
+					addEdge(i, it->snd);
+					addEdge(it->snd, i);
+				}
+	
+			in.insert(mp(mp(P[i].snd, P[i].fst), i));
 		}
-
-	printf("%d\n", ans);
+	
+		int ans = 0;
+		for (i = 0; i < N; i++)
+			if (!v[i]){
+				cnt = a = 0;
+				bipartite(i, 0);
+				ans += min(a, cnt - a);
+			}
+	
+		printf("%d\n", ans);
+	}
 }
